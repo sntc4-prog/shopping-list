c9832d816514af3187c95f3d70353d4dd8ce7466
Egor <egorborisovzypo@gmail.com>
2022-02-05 21:22:01 +0600

    Initial commit

---
+++ src/main/java/ru/sntc4/shoppinglist/demo/application/ShoppingListApplication.java
@@ -0,0 +1,13 @@
+package ru.sntc4.shoppinglist.demo.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingListApplication.class, args);
+	}
+
+}
